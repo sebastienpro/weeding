52a38f1aceb9860bd1c9df557367b0abfb8d973d
sebastienpro <spro@webgeoservices.com>
2016-07-16 09:54:21 +0200

    don't use resized image in preview

--- book/views.py
+++ book/views.py
@@ -12,9 +12,6 @@ from mariage import settings
 
 def home(request):
     pages = Page.objects.all().order_by("-pk")
-    #page_size = CSS(string='@page { size: A4; margin: 1cm }')
-    #HTML('http://127.0.0.1:8000/preview/25/').write_pdf(os.path.join(settings.STATIC_ROOT, "pdfs","test.pdf"), stylesheets=[page_size])
-    #pdfkit.from_url('http://127.0.0.1:8000/preview/25/', os.path.join(settings.STATIC_ROOT, "pdfs","test.pdf"))
     return render(request, "home.html", {'pages': pages})
 
 
@@ -54,9 +51,9 @@ def page_edit(request, page_id):
 
 def preview(request, page_id):
     page = Page.objects.get(pk=page_id)
-    new_image1_url = _resize_image(page.image1)
-    new_image2_url = _resize_image(page.image2)
-    return render(request, "preview.html", {'page': page, 'image1': new_image1_url, 'image2': page.image2})
+    #new_image1_url = _resize_image(page.image1)
+    #new_image2_url = _resize_image(page.image2)
+    return render(request, "preview.html", {'page': page, 'image1': page.image1, 'image2': page.image2})
 
 def _resize_image(image):
     new_image = image.name.replace("images/", "resized/")
